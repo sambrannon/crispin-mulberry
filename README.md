90e8b96eb9ff2c93f0458306408b9350d0b22c97
Sam Brannon <sbrannon-contractor@cars.com>
2016-05-22 18:15:25 -0500

    Started adding styles for header and nav

---
+++ README.md
@@ -0,0 +1,9 @@
+# Crispin Mulberry
+
+A small website made for a skills test
+
+## Notes
+
+- I did not have access to Photoshop for this project
+- Because of the above I used Google to find other applicants versions of this to get some assets that I assume were slices in the PSD (i.e. logo)
+- CSS gradients generated with http://www.gradientapp.com/
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,33 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var scsslint = require('gulp-scss-lint');
+var nmq = require('gulp-no-media-queries');
+var rename = require("gulp-rename");
 
 gulp.task('styles', function () {
   gulp.src('assets/scss/**/*.scss')
+    .pipe(scsslint({
+        'config': '.scss-lint.yml',
+    }))
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('assets/css'));
 });
 
+gulp.task('nmq', function () {
+  gulp.src('./assets/css/styles.css')
+  .pipe(nmq({ 'width': '1000px' }))
+  .pipe(rename({
+    suffix: '.ie',
+    extname: '.css'
+  }))
+  .pipe(gulp.dest('./assets/css/'));
+});
+
 gulp.task('watch', function(){
-  gulp.watch('assets/scss/**/*.scss', ['styles']);
+  gulp.watch('assets/scss/**/*.scss', ['styles', 'nmq']);
 });
 
 gulp.task('default', ['watch']);
